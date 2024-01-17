9dac79388a542545cd8800f4da936783cc276423
sandhals <brixtonsandhals@gmail.com>
2024-01-17 16:10:28 +0900

    changed class names and added easter egg

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-//Change timeZone to current location to provide updated status!!!
+// Function for getting time and status for the marquee
+// Just edit 'Asia/Seoul' when on vacation somewhere!
 const currentTimeZone = new Intl.DateTimeFormat('en-US', { timeZone: 'Asia/Seoul' }).resolvedOptions().timeZone;
 const currentCity = currentTimeZone.split('/')[1];
 
-// Function to update time and status
 function updateInfo() {
   const currentTime = new Date(new Date().toLocaleString('en-US', { timeZone: currentTimeZone }));
   const hours = currentTime.getHours();
@@ -13,7 +13,7 @@ function updateInfo() {
   const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
   let statusMessage = '';
 
-  // Determine the status based on the time
+// Various statuses depending on the time
   if ((currentTime.getDay() >= 1 && currentTime.getDay() <= 5 && hours >= 8 && hours < 16.5)) {
     statusMessage = '\u00A0AT\u00A0WORK\u00A0';
   } else if ((currentTime.getDay() >= 1 && currentTime.getDay() <= 5 && hours >= 16.5 && hours < 18) ||
@@ -34,7 +34,6 @@ function updateInfo() {
     statusMessage = '\u00A0AT\u00A0HOME\u00A0';
   }
 
-  // Update the HTML elements
   if (currentCity === "Seoul") {
     document.getElementById('currentTime').innerText = '\u00A0' + formattedTime + '\u00A0';
     document.getElementById('currentWeekday').innerText = '\u00A0' + today + '\u00A0';
@@ -44,26 +43,71 @@ function updateInfo() {
   }
 }
 
-// Update every second
+// Update time every second
 setInterval(updateInfo, 1000);
 
 
-
-
-
-
-
-
-
+// Grab the source
     document.addEventListener('DOMContentLoaded', function() {
-        // Fetch the source code of the current page
         fetch(window.location.href)
             .then(response => response.text())
             .then(html => {
-                // Display the source code in the pre element
                 document.getElementById('sourceCode').textContent = html;
             })
             .catch(error => {
                 console.error('Error fetching source code:', error);
             });
     });
+
+// Function for toggling the source code on and off
+        document.addEventListener('DOMContentLoaded', function () {
+          var content = document.querySelector('.content');
+          var sourceCodeContainer = document.querySelector('.sourcecode');
+          var showSource = document.querySelector('.showsource');
+          var container = document.querySelector('.container');
+// Toggle to show  
+          function showSourceCode() {
+              sourceCodeContainer.style.filter = 'blur(0px)';
+              content.style.display = 'none';
+              sourceCodeContainer.style.position = 'static';
+              container.style.overflowY = 'visible';
+  
+              var hideSource = document.createElement('div');
+              hideSource.className = 'hidesource';
+              hideSource.innerText = 'this site was handmade with love 💙';
+              hideSource.addEventListener('click', hideSourceCode);
+              document.body.appendChild(hideSource);
+
+              window.scrollTo(0, 0);
+          }
+// Toggle to hide  
+          function hideSourceCode() {
+              sourceCodeContainer.style.filter = 'blur(1px)';
+              content.style.display = 'block';
+              sourceCodeContainer.style.position = 'absolute';
+              container.style.overflow = 'hidden';
+
+              window.scrollTo(0, document.body.scrollHeight || document.documentElement.scrollHeight);
+
+
+              var hideSource = document.querySelector('.hidesource');
+              if (hideSource) {
+                  hideSource.remove();
+              }
+          }
+
+          showSource.addEventListener('click', showSourceCode);
+      });
+
+      window.onload = function () {
+          var sourceCodeContainer = document.getElementById('sourceCodeContainer');
+
+          fetch('index.html')
+              .then(response => response.text())
+              .then(sourceCode => {
+                  sourceCodeContainer.innerText = sourceCode;
+              })
+              .catch(error => {
+                  console.error('Error fetching source code:', error);
+              });    
+      };
